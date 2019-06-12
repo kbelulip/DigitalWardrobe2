45c9f1bf36d898185100cb9811f5180890647dd6
kbelulip <k.belulipaez@smail.uni-koeln.de>
2019-06-12 17:05:44 +0200

    TESTESTEST

--- app/src/main/java/com/example/digitalwardrobe2/MainActivity.java
+++ app/src/main/java/com/example/digitalwardrobe2/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.digitalwardrobe2;
 
 import android.content.Intent;
-import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    RecyclerView mRecyclerView;
+    MyAdapter mAdapter;
+
+    private ArrayList<ListPetsQuery.Item> mPets;
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Textfeld wird fett ausgegeben
-        // TextView textView_header = (TextView) findViewById(R.id.textView_header);
-        // textView_header.setPaintFlags(textView_header.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-
         Button buttonMenue = (Button) findViewById(R.id.button_Menue);
         buttonMenue.setOnClickListener(this);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        // use a linear layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ClientFactory.init(this);
     }
 
 
